b56f7f65352446c52becbf677603cd5584548804
Francisco Noriega <frannoriega.92@gmail.com>
2026-02-23 11:32:34 -0300

    fix: permitir headers para cloudflare

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,23 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: `
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://challenges.cloudflare.com;
+              frame-src 'self' https://challenges.cloudflare.com;
+              connect-src 'self' https://challenges.cloudflare.com;
+            `.replace(/\s{2,}/g, ' ').trim()
+          }
+        ],
+      },
+    ]
+  }
 };
 
 const withMDX = createMDX({
