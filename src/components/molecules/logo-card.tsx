ec3f9ff74204c19efbb9871cf975309ee1c89800
Francisco Noriega <frannoriega.92@gmail.com>
2026-07-06 16:50:38 -0300

    revert: logos de miembros/socios con fondo blanco
    Esto se cambio para dark mode, pero no se ve bien porque los logos
    son de mala calidad.
    No queda otra que mostrar con fondo blanco.

--- src/components/molecules/logo-card.tsx
+++ src/components/molecules/logo-card.tsx
@@ -12,17 +12,17 @@ export function LogoCard({ name, img, url }: LogoCardProps) {
   return (
     <div className="mx-2">
       <Link href={url} target="_blank" className="block">
-        <Card className="flex flex-col gap-4 bg-card grayscale transition-all duration-300 hover:grayscale-0 hover:scale-105">
+        <Card className="flex flex-col gap-4 bg-white grayscale transition-all duration-300 hover:grayscale-0 hover:scale-105">
           <CardHeader className="sr-only text-center w-full">
             <CardTitle className="text-2xl">{name}</CardTitle>
           </CardHeader>
-          <CardContent className="h-[200px] flex flex-col items-center justify-center rounded-md">
+          <CardContent className="h-50 flex flex-col items-center justify-center rounded-md">
             <Image
               src={img}
               alt={name}
               width={200}
               height={200}
-              className="object-contain h-full rounded-md"
+              className="object-contain h-full rounded-md bg-transparent"
             />
           </CardContent>
         </Card>
